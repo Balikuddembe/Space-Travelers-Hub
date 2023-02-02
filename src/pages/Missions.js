6d875661309f9a33f52a0c3543710e9b7fc66ad4
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-02-01 23:34:59 -0500

    Render missions in table and style all (more)

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -7,44 +7,50 @@ const Missions = () => {
   const dispatch = useDispatch();
   const state = true;
 
-  const missions = useSelector((state) => state.Missions.data)
+  const missions = useSelector((state) => state.Missions.data);
   useEffect(() => {
     dispatch(getMissions());
   });
 
-  const joinBtn = () => {
-   return state ? {display: 'none'} : {display: 'block'};
-  }
+  const joinBtn = () => (state ? { display: 'none' } : { display: 'block' });
 
-  const leaveBtn = () => {
-    return state ? {display: 'block'} : {display: 'none'};
-  }
+  const leaveBtn = () => (state ? { display: 'block' } : { display: 'none' });
 
   return (
     <div id="table-container">
       <table className="missions-table">
-       <thead>
-         <tr>
-           <th>Mission</th>
-           <th>Description</th>
-           <th>Status</th>
-           <th></th>
-         </tr>
-       </thead>
-       <tbody>
-         {missions.map((mission) => {
-          return (
-          <tr key={mission.mission_id} id={mission.mission_id}>
-            <td className="mission"> { mission.mission_name} </td>
-            <td className="description"> { mission.description } </td>
-            <td className="statusContainer"> <div className="status">NOT A MEMBER </div></td>
-            <td className="joinContainer"> 
-              <button className="joinBtn" style={joinBtn()}>Join Mission</button>
-              <button className="leaveBtn" style={leaveBtn()}>Leave Mission</button> 
-            </td>
-          </tr>)
-         })}
-       </tbody>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>{' '}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <tr key={mission.mission_id} id={mission.mission_id}>
+              <td className="mission">
+                {' '}
+                { mission.mission_name}
+                {' '}
+              </td>
+              <td className="description">
+                {' '}
+                { mission.mission_description }
+                {' '}
+              </td>
+              <td className="statusContainer">
+                {' '}
+                <div className="status">NOT A MEMBER </div>
+              </td>
+              <td className="joinContainer">
+                <button type="button" className="joinBtn" style={joinBtn()}>Join Mission</button>
+                <button type="button" className="leaveBtn" style={leaveBtn()}>Leave Mission</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
