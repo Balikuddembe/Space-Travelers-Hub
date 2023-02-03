d23be7374472d180d8cb00237e1e1b9fec69b043
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-02-03 11:17:55 -0500

    Add MissionProfile - Show My Missions in MyProfile Page

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import RocketsProfile from '../components/Profile/RocketsProfile';
+import MissionsProfile from '../components/Profile/MissionsProfile';
 
 const MyProfile = () => (
   <div className="profile-container">
     <RocketsProfile />
+    <MissionsProfile />
   </div>
 );
 
