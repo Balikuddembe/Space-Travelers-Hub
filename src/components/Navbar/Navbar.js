dfaca3be1d58264efd23b19673fee925c7ee5704
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-02-03 22:17:32 -0500

    Use semantic headers

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,17 +5,17 @@ import Logo from '../../Images/planet.png';
 
 function NavBar() {
   return (
-    <div className="NavBar">
+    <header className="NavBar">
       <div className="LogoContainer">
-        <img className="planet" src={Logo} alt="" />
+        <img className="planet" src={Logo} alt="logo planet" />
         <h1>Space Traverlers Hub</h1>
       </div>
-      <div className="NavLinks">
+      <nav className="NavLinks">
         <NavLink to="/" end className={({ isActive }) => (isActive ? 'currentPage' : undefined)}>Rockets</NavLink>
         <NavLink to="./Missions" end className={({ isActive }) => (isActive ? 'currentPage' : undefined)}>Missions</NavLink>
         <NavLink to="./MyProfile" end className={({ isActive }) => (isActive ? 'currentPage' : undefined)}>My Profile</NavLink>
-      </div>
-    </div>
+      </nav>
+    </header>
   );
 }
 
